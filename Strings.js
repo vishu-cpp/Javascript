97964461f6c2fc800c1b0481482c0f5d7356e49f
vishu-cpp <vishu12113@gmail.com>
2026-08-10 20:46:51 +0530

    Strings covered

---
+++ Strings.js
@@ -0,0 +1,25 @@
+console.log("Vishesh" + "Morning")
+
+
+let name = "Vishesh";
+let age = 21;
+
+console.log(`My name is ${name} and I am ${age} years old`);
+
+
+const newname = new String("Vishesh");
+console.log(newname);
+console.log(typeof newname); //object
+
+console.log(newname.toUpperCase());
+console.log(newname.toLowerCase());
+console.log(newname.charAt(0));
+console.log(newname.indexOf("h"));
+console.log(newname.substring(0, 3));
+console.log(newname.slice(0, 3));
+console.log(newname.trim());
+console.log(newname.replace("Vishesh", "Vishesh"));
+console.log(newname.split(" "));
+console.log(newname.split(""));
+console.log(newname.split("e"));
+console.log(newname.split("h"));
